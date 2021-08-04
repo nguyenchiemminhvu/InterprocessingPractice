06fc0562d80a568eac74d07b3f9eabc748501701
vu3.nguyen <vu3.nguyen@lge.com>
2021-08-04 21:56:16 +0700

    update

---
+++ shared_memory/mem_reader.cpp
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <shmem/shmem.h>
+#include <semaphore.h>
+
+int main()
+{
+    
+
+    return 0;
+}
